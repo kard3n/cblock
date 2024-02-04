7fb55dc4a50e591faa70a1fc9e02129b5f813ae7
kard3n <jonathan.preiss@yahoo.de>
2024-02-04 17:02:29 +0100

    Fix json_schema_parser.py

--- cblock/utils/json_schema_parser.py
+++ cblock/utils/json_schema_parser.py
@@ -6,155 +6,216 @@ from utils.SchemaParsingException import SchemaParsingException
 
 class JsonSchemaParser:
 
+    # returns new pos after all whitespaces and linebreaks
+    @classmethod
+    def __jump_whitespaces_linebreaks(cls, string: str, pos: int) -> int:
+        while string[pos] in ['\n', " "]:
+            pos += 1
+
+        return pos
+
+    # extracts an element, pos is the start pos
+    @classmethod
+    def __extract_item(cls, input: str, pos: int) -> int:
+        open_dividers: int = 0
+
+
+
+
     @classmethod
     def parse_schema(cls, schema: str) -> ElementContainer:
-        #result: dict = {}
-        #item_name: str = ''
-        element_container: ElementContainer = ElementContainer()
-
         pos: int = 0
 
-        # jump all linebreaks and whitespaces
-        while schema[pos] in ['\n', " "]:
-            pos += 1
+        cls.__jump_whitespaces_linebreaks(schema, pos)
 
-        """if schema[pos] == '"':
-            pos += 1
-            while schema[pos] != '"' and schema[pos -1] != '\\':
-                item_name += schema[pos]
-                pos += 1
-
-        if schema[pos] != '"':
-            raise SchemaParsingException(f"Unexpected character {schema[pos]} at position {pos}")
-
-
-        result[item_name] = cls.__parse_schema_element(schema[pos +1:], comes_with_tags=True)"""
-
-        return cls.__parse_schema_element(schema[pos:], comes_with_tags=False)
+        return cls.__parse_schema_element(schema[pos:])
 
     # converts the received element to an ElementContainer. element must start with the tags followed by a ":"
     # and then the content (list, dict or string)
     # if element does not start with tags followed by a ':', then comes_with_tags needs to be set to False
     @classmethod
-    def __parse_schema_element(cls, element: str, inherited_tags: list[ContentTag] = None,
-                             comes_with_tags: bool = True) -> ElementContainer:
+    def __parse_schema_element(cls, element: str, inherited_tags: list[ContentTag] = None) -> ElementContainer:
         element_container: ElementContainer = ElementContainer()
 
-        #print(f"received element {element}")
+
         # add inherited tags to initially empty tag list
         if inherited_tags is not None:
             element_container.tags = deepcopy(inherited_tags)
 
         pos: int = 0
 
-
-        # if element has tags at the beginning, we add them
-        if comes_with_tags:
+        # check if the passed element has tags, if it does adds them
+        if element[pos].isalpha() or element[pos] == ':':
             while element[pos] != ':':
                 if ContentTag(element[pos]) in ContentTag:
                     element_container.tags.append(ContentTag[ContentTag(element[pos]).name])
                     pos += 1
                 else:
-                    raise SchemaParsingException(f'Invalid content tag \"schema[pos]\" at position {pos}')
+                    raise SchemaParsingException(f'Invalid content tag \"{element[pos]}\" at position {pos}')
 
-            # jump over the ":"
+            # jump ':'
             pos += 1
-        # jump all linebreaks and whitespaces
-        while element[pos] in ['\n', " "]:
-            pos += 1
-        # it's text, we get it and add it as value
+
+        pos = cls.__jump_whitespaces_linebreaks(element, pos)
+
+        # tags have been read, now check for type of the value
+        # it starts with a quote, so it must be a string
         if element[pos] == '"':
-            element_container.value = ""
             element_container.value_type = ValueType.LEAF
+            element_container.value = ''
+
+            # jump quote
             pos += 1
-            # add to value until a not-escaped quotation mark is found
-            while element[pos] != '"' and element[pos - 1] != '\\':
+
+            while pos < len(element) and element[pos] != '"' and element[pos -1] != '\\':
                 element_container.value += element[pos]
                 pos += 1
 
-        # it's a list, we get the content and parse it to ElementContainer
-        # a list, since all elements have the same structure, contains only one element that will get parsed
-        elif element[pos] == '[':
-            element_container.value = ElementContainer()
-            element_container.value_type = ValueType.LIST
-            open_dividers: int = 1  # counts the currently non-closed dividers such as {} and []
-            child_value: str = ''
-
+            # jump quote
             pos += 1
 
-            # jump all linebreaks and whitespaces
-            while element[pos] in ['\n', " "]:
-                pos += 1
-            while open_dividers > 0:
-                if element[pos] in ['[', '{'] and element[pos - 1] != '\\':
-                    open_dividers += 1
-                elif element[pos] in [']', '}'] and element[pos - 1] != '\\':
-                    open_dividers -= 1
+            if pos < len(element):
+                raise SchemaParsingException(f'Unexpected character \"{element[pos]}\" at relative position {pos}')
 
-                if open_dividers > 0:
-                    child_value += element[pos]
-
+        # starts with a bracket, must be a list
+        elif element[pos] == '[':
+            element_container.value_type = ValueType.LIST
+            current_value: str = ''
+            opened_lists: int = 1
+            while not(element[pos] == ']' and element[pos-1] != '\\' and opened_lists == 0):
                 pos += 1
-            element_container.value = cls.__parse_schema_element(child_value, element_container.tags, False)
-        # it's a dictionary. all elements need to be parsed to ElementContainer and saved
-        # with their respective names
+
+                if element[pos] == '[' and element[pos-1] != '\\':
+                    opened_lists += 1
+                elif element[pos] == ']' and element[pos-1] != '\\':
+                    opened_lists -= 1
+
+
+
+                current_value += element[pos]
+            element_container.value = cls.__parse_schema_element(current_value[0: len(current_value)-1], element_container.tags)
+
+
+
+        # starts with a curly bracket, must be a dictionary
         elif element[pos] == '{':
-            #print(f"It's a dictionary: {element[pos:]}")
             element_container.value_type = ValueType.DICT
             element_container.value = {}
-            open_dividers: int = 1  # counts the currently non-closed dividers such as {} and []
-            current_child_value: str = ''
-            current_item_name: str = ''
+            open_dividers: int = 0
 
-            # jump '{'
+            current_value: str = ''
+            current_name: str = ''
+
+            current_divider_open: str = ''
+
+            current_divider_close: str = ''
+
+            # jump curly bracket
             pos += 1
 
-            # jump all linebreaks and whitespaces
-            while element[pos] in ['\n', " "]:
-                pos += 1
-
-            # get the name
-            if element[pos] == '"':
-                pos += 1
-                while element[pos] != '"' and element[pos - 1] != '\\':
-                    current_item_name += element[pos]
-                    pos += 1
-
-            #print(f"Current item name: {current_item_name}")
-
-            if element[pos] != '"':
-                raise SchemaParsingException(f"Unexpected character {element[pos]} at position {pos}")
-
-            # jump remaining '"'
-            pos += 1
-
-            # jump all linebreaks and whitespaces
-            while element[pos] in ['\n', " "]:
-                pos += 1
-
-            #print(f"Currently remaining: {element[pos:]}")
-
-            # extract all text until the comma that denotes the end of the element
-            while open_dividers > 0:
-                #print(f"Currently remaining: {element[pos:]} with {open_dividers} open dividers. Current child value: {current_child_value}. Pos: {element[pos]}")
-
-
-
-
-                if open_dividers > 0:
-                    current_child_value += element[pos]
-                if open_dividers == 1 and element[pos] == '}': # or element[pos] == '}'
-                    #print(f"current_child_value 1: {current_child_value}")
-                    element_container.value[current_item_name] = (
-                        cls.__parse_schema_element(current_child_value,
-                                                    element_container.tags,
-                                                    True))
-
-                if element[pos] in ['[', '{'] and element[pos - 1] != '\\':
+            while not(element[pos] == '}' and element[pos-1] != '\\' and open_dividers == 0):
+                if element[pos] == '}' and element[pos-1] != '\\':
                     open_dividers += 1
-                elif element[pos] in [']', '}'] and element[pos - 1] != '\\':
+                elif element[pos] == '{' and element[pos-1] != '\\':
                     open_dividers -= 1
 
-                pos += 1
+                pos = cls.__jump_whitespaces_linebreaks(element, pos)
+
+                # if there's a comma, jump over it
+                if element[pos] == ',':
+                    pos += 1
+                    pos = cls.__jump_whitespaces_linebreaks(element, pos)
+
+                # found a quotation mark, so we got a new name and therefore a new element
+                if element[pos] == '"':
+                    # extract name
+                    end_of_name: int = cls.__get_name(element, pos)
+                    current_name = element[pos+1: end_of_name]
+
+                    # extract value, +1 to jump over quotation mark
+                    pos = end_of_name + 1
+
+                    pos = cls.__jump_whitespaces_linebreaks(element, pos)
+
+                    while pos < len(element) and not element[pos] in ['"', '{', '[']:
+
+                        current_value += element[pos]
+                        pos +=1
+
+                    if not element[pos] in ['"', '{', '[']:
+                        raise ValueError(f"The following character does not correspond to a type: {element[pos]}")
+
+                    current_divider_open = element[pos]
+
+                    if current_divider_open == '{':
+                        current_divider_close = '}'
+                    elif current_divider_open == '"':
+                        current_divider_close = '"'
+                    elif current_divider_open == '[':
+                        current_divider_close = ']'
+
+                    open_dividers = 0
+
+                    if current_divider_open == '"':
+                        current_value += element[pos]
+                        pos += 1
+                        while not(element[pos] == current_divider_close and element[pos-1] != '\\'):
+                            current_value += element[pos]
+                            pos += 1
+
+                        # add close divider
+                        current_value += element[pos]
+                        pos += 1
+                    else:
+                        # add to child value until end of current value
+                        pos -= 1
+                        while not(open_dividers == 0 and element[pos] == current_divider_close and element[pos-1] != '\\'):
+                            pos += 1
+                            if element[pos] == current_divider_close and element[pos - 1] != '\\':
+                                open_dividers -= 1
+                            elif element[pos] == current_divider_open and element[pos - 1] != '\\':
+                                open_dividers += 1
+
+                            current_value += element[pos]
+
+                        pos += 1
+
+                element_container.value[current_name] = cls.__parse_schema_element(current_value, element_container.tags)
+                current_value = ''
+
+                # remove comma after last element, if it exists.
+                # Otherwise, the loop won't be able to check for closing the divider '}'
+                pos = cls.__jump_whitespaces_linebreaks(element, pos)
+                if element[pos] == ',':
+                    pos += 1
+                pos = cls.__jump_whitespaces_linebreaks(element, pos)
+
+
+        else:
+            raise SchemaParsingException(f'Symbol does not correspond to a type: {element[pos]}')
 
         return element_container
+
+
+
+    @classmethod
+    def __get_name(cls, input: str, start_pos: int) -> int:
+        pos: int = start_pos
+
+
+        pos = cls.__jump_whitespaces_linebreaks(input, pos)
+
+        if input[pos] != '"':
+            raise SchemaParsingException(f'Could not determine beginning of name \"{input[start_pos:]}\". No ' +
+                                         'quotation mark could be found.')
+
+        # jump quotation mark
+        pos += 1
+
+        while pos < len(input) and not(input[pos] == '"' and input[pos-1] != '\\'):
+            pos += 1
+
+        if input[pos] != '"':
+            raise SchemaParsingException(f'Could not determine ending of name \"{input[start_pos:]}\".')
+
+        return pos
--- cblock/utils/test.py
+++ cblock/utils/test.py
@@ -1,15 +1,24 @@
 from src.content_editor.editors.json_editor.ContentTag import ContentTag
 from utils.json_schema_parser import JsonSchemaParser
 
-schema: str = '''{"firstName"t: "John"}'''
+schema: str = '''{"second_name"t: {"dicty": "Big"}, "firstName"t: "John", }'''
+schema_: str = '''{"second_name"t: {"firstName"t: "John", "dicty": "Big"}, }'''
+
 schema2: str = '''{"contained_list"r: [{"picture"p:"pic.jpg",}]}"'''
 
-schema3: str = '''{"contained_dictionary"r: {"picture"p:"pic.jpg",}}"'''
+schema3: str = '''{"contained_dictionary"r:{"picture"p:"pic.jpg,"},}"'''
+schema4: str = '''{"contained_dictionary"r:{"dict2":{"firstName"t: "John"},"picture"p:"pic.jpg",}}"'''
 
 #print(ContentTag('r').name)
 
+print([e.value for e in ContentTag])
+
 print(f"Result: {JsonSchemaParser.parse_schema(schema)}\n")
+
+print(f"Result: {JsonSchemaParser.parse_schema(schema_)}\n")
 
 print(f"Result: {JsonSchemaParser.parse_schema(schema2)}\n")
 
 print(f"Result: {JsonSchemaParser.parse_schema(schema3)}\n")
+
+print(f"Result: {JsonSchemaParser.parse_schema(schema4)}")
