732183759b5268366cd956e9bf53428047b8e546
kard3n <jonathan.preiss@yahoo.de>
2024-03-19 23:31:46 +0100

    feat: delete database if initialization fails

--- cblock/src/cblock.py
+++ cblock/src/cblock.py
@@ -2,6 +2,7 @@
 Entry point for CBlock
 
 mitmproxy -m regular -s cblock/src/cblock.py
+mitmproxy -m regular --set console_eventlog_verbosity=warn -s cblock/src/cblock.py
 
 
 Enable utf-8 support (needs to be done only once per environment):
@@ -9,6 +10,7 @@ conda env config vars set PYTHONUTF8=1
 """
 
 import logging
+import os
 
 import regex
 
@@ -55,19 +57,21 @@ class Main:
                 )
                 schema_reader.run()
             except Exception as e:
-                logging.error(f"Error while initializing database: {e.__traceback__}")
+                os.remove("cb_database.db")
+                logging.error(f"Error while initializing database: {e}")
 
     async def response(self, flow: http.HTTPFlow):
         # logging.warning(f"URI: {flow.request.pretty_host + flow.request.path}")
         # logging.warning(f"Pretty host: {flow.request.pretty_host}")
         paths: list[PathSearchResult] = self.db_manager.get_paths_for_url(
-            url=flow.request.pretty_host
+            url=flow.request.pretty_host.removeprefix("www.")
         )
 
-        logging.warning(f"Paths: {paths}")
+        # logging.warning(f"Paths for {flow.request.pretty_host.removeprefix("www.")}: {paths}")
 
         for search_result in paths:
             if regex.compile(search_result.path).match(flow.request.path) is not None:
+                logging.warning(f"Paths for {flow.request.pretty_host.removeprefix("www.")}: {paths}")
                 flow.response.text = await self.__edit(
                     schema_id=search_result.id,
                     content=flow.response.text,
