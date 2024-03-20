327118e094513429e01bff92b93948c8a1e6528b
kard3n <jonathan.preiss@yahoo.de>
2024-03-20 14:32:39 +0100

    feat: improve default Content values

--- cblock/src/content_factory/Content.py
+++ cblock/src/content_factory/Content.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Content:
-    title: str = "Title"
-    subtitle: str = "Subtitle"
-    summary: str = "Summary"
-    full: str = "Full Content"  # Example: an article
+    title: str = "Title removed by CBlock"
+    subtitle: str = "Subtitle removed by CBlock"
+    summary: str = "Summary removed by CBlock"
+    full: str = "Full Content removed by CBlock"  # Example: an article
     picture: str = "picture.png"
     video: str = "video.mp4"
     audio: str = "audio.mp3"
--- cblock/src/content_factory/ContentFactory.py
+++ cblock/src/content_factory/ContentFactory.py
@@ -4,10 +4,10 @@ from content_factory.Content import Content
 class ContentFactory:
     def get_content(self) -> Content:
         return Content(
-            title="Title",
-            subtitle="Subtitle",
-            summary="Pretend this is a summary.",
-            full="This is a long article, that could fill multiple pages.",
+            title="Title removed by ContentBlock",
+            subtitle="Subtitle removed by ContentBlock",
+            summary="Summary removed by ContentBlock.",
+            full="Full text removed by ContentBlock.",
             picture="picture.png",
             video="video.mp4",
             audio="audio.wav",
