3c36111c6350557754fece3762366d17fed59fe0
kard3n <jonathan.preiss@yahoo.de>
2024-05-03 16:31:51 +0200

    fix: create save classifier pickle in right place

--- classifier_scripts/build_train_classifier.py
+++ classifier_scripts/build_train_classifier.py
@@ -13,9 +13,7 @@ def create_classifier(dataset_location: str | None = None):
     print("Creating dataset...")
     if dataset_location is None:
         dataset_location = (
-            pathlib.Path(__file__).parent.resolve().as_posix()
-            + "/"
-            + "2024_04_05_to_2024_04_17.csv"
+            pathlib.Path(__file__).parent.resolve().as_posix() + "/" + "dataset.csv"
         )
     dataset = create_dataset(ds_location=dataset_location, only_title=False)
 
@@ -25,7 +23,10 @@ def create_classifier(dataset_location: str | None = None):
     pipeline = ClassifierPipeline(stem_input=True, stem_tokens=False)
     pipeline.train(dataset_x, dataset_y)
 
-    with open("classifier.pickle", "wb") as handle:
+    with open(
+        "../cblock/content_classifier/classifiers/naive_bayes/classifier.pickle",
+        "wb",
+    ) as handle:
         pickle.dump(pipeline, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     pipeline.test_training(dataset_x, dataset_y)
