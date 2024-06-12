91d1683dd446c09cdfdebfd29130486f2728a2cf
kard3n <jonathan.preiss@yahoo.de>
2024-06-12 19:28:53 +0200

    feat: remove redundant Schema class

--- cblock/schema/SchemaType.py
+++ cblock/schema/SchemaType.py
@@ -1,25 +1,10 @@
-from dataclasses import dataclass
 from enum import Enum
-
-from schema.generic_schema.GenericSchema import GenericSchema
-from schema.html_schema.HTMLSchema import HTMLSchema
-from schema.json_schema.JSONSchema import JSONSchema
 
 
 class SchemaType(str, Enum):
     GENERIC = "generic"
     JSON = "json"
     HTML = "html"
-
-
-@dataclass
-class Schema:
-    id: str
-    url: str = None
-    path: str = None
-    schema_type: SchemaType = None
-    schema: JSONSchema | GenericSchema | HTMLSchema = None
-
 
 # The name of the file is the schema's name. The ID is not specified by the user, the app automatically assigns one
 # The user-definition also uses names, which when parsed get converted to IDs
